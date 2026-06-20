2c4501ca38ef2017512cb46ee0f9e7c6618fdb5e
Julio Henrique <dev@juliohenrique.com>
2026-06-20 17:52:02 -0300

    That's it for now, again

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <cstdlib>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 
 // socket function call
 // int socket(int domain, int type, int protocol);
 
-int main(int argc, char **argv) {
-    
+int main(int argc, char** argv) {
+
     if (argc == 1) {
         std::exit(1);
     }
 
-
+    // Creates an epoll instance, and avoids leaking its instance by using the only valid flag
+    // available: "EPOLL_CLOEXEC". This flags instructs the closure of this instance to close itself
+    // if the process running changes when using the exec() function. If you pass 0, it won't close.
+    int epoll_instance = epoll_create1(EPOLL_CLOEXEC);
 }
